20e44af0a9cf206e2da1250566236c3862b9e3bc
Ankitsinghsisodya <ankitsingh24012005@gmail.com>
2024-12-10 21:13:01 +0530

    Added timer

--- script.js
+++ script.js
@@ -11,69 +11,83 @@ async function getSongs() {
   }
 }
 
-let prev = "Nothing";
-let flag = false;
-
-async function main() {
-  let currentSong;
-  let songs = await getSongs();
-  console.log(songs);
-
-  // show all the songs int he playlist
+let currentSong = new Audio();
+function showAllSongs(songs) {
   let songUl = document
     .querySelector(".song-list")
     .getElementsByTagName("ul")[0];
-  console.log(songs);
+
   for (const song of songs) {
     songUl.innerHTML =
       songUl.innerHTML +
       ` <li class="flex gap-x-3 justify-between border rounded-md p-2 text-white
-      bg-gradient-to-r from-yellow-500 to-red-500">
-                            <img src="./img/music.svg" alt="">
-                            <span class="gap-x-2">${song.title}</span>
-                            <img src="./img/play.svg" alt="">
-                        </li>`;
-    console.log(songUl.innerHTML);
+    bg-gradient-to-r from-yellow-500 to-red-500">
+                          <img src="./img/music.svg" alt="">
+                          <span class="gap-x-2">${song.title}</span>
+                          <img src="./img/play.svg" alt="">
+                      </li>`;
   }
+}
+async function main() {
+  let songs = await getSongs();
 
-  document
-    .querySelector(".play-icon")
-    .addEventListener("click", () => {
-      if (flag == true) {
-        prev.pause();
-      }
-    });
+  // show all the songs int he playlist
+
+  showAllSongs(songs);
+
   //Attach even listener to each song
   Array.from(
     document.querySelector(".song-list").getElementsByTagName("li")
   ).forEach((e) => {
     e.addEventListener("click", (event) => {
-      console.log(e.getElementsByTagName("span")[0].textContent);
       playSong(e.getElementsByTagName("span")[0].textContent);
     });
   });
-}
 
-async function playSong(song) {
-  const songs = await getSongs();
-  console.log(song, "playsong ke andar");
-  for (const songObj of songs) {
-    console.log(song);
-    if (song === songObj.title) {
-      if (flag) {
-        prev.pause();
-        prev.currentTime = 0;
-      }
-      flag = true;
-
-      let audio = new Audio("./Public/" + songObj.file);
-      prev = audio;
-      console.log("./Public/" + songObj.file);
-      audio.play();
-      break;
+  // Attaching event listener of play and pause at seekbar
+  play.addEventListener("click", () => {
+    if (currentSong.paused) {
+      currentSong.play();
+      play.src = "./img/pause.svg";
+    } else {
+      currentSong.pause();
+      play.src = "./img/play.svg";
     }
-  }
+  });
 }
+
+function playSong(song) {
+  getSongs().then((songs) => {
+    for (const songObj of songs) {
+      if (song === songObj.title) {
+        currentSong.src = "./Public/" + songObj.file;
+
+       
+        // Update the current time dynamically
+        currentSong.addEventListener("timeupdate", () => {
+          const currentTime = formatTime(currentSong.currentTime);
+          const totalDuration = formatTime(currentSong.duration);
+
+          // Update the current time dynamically
+          document.querySelector(
+            ".songTime"
+          ).innerHTML = `<p>${currentTime} / ${totalDuration}
+            <span class='text-purple-900'>${songObj.title}</span></p>`;
+        });
+
+        play.src = "./img/pause.svg";
+        currentSong.play();
+        break;
+      }
+    }
+  });
+}
+function formatTime(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const secs = Math.floor(seconds % 60);
+  return `${minutes}:${secs < 10 ? "0" : ""}${secs}`;
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   main();
 });
