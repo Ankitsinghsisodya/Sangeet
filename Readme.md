c2f166466cad705af5a4e742be34b0dce258fb18
Ankitsinghsisodya <ankitsingh24012005@gmail.com>
2024-12-10 18:46:52 +0530

    updating readme.md

---
+++ Readme.md
@@ -0,0 +1,29 @@
+# Music System
+
+This is a  web application that allows users to view and play Bhaajan from a collection of songs.
+
+## Features
+
+- Fetches songs from a JSON file.
+- Displays a list of songs with a gradient background.
+- Allows users to play songs by clicking on them.
+
+## Technologies Used
+
+- HTML
+- CSS (Tailwind CSS)
+- JavaScript
+
+## Getting Started
+
+### Prerequisites
+
+- A web browser
+- A local server (optional, for better performance)
+
+### Installation
+
+1. Clone the repository:
+
+```sh
+git clone https://github.com/your-username/music-system.git
---
+++ script.js
@@ -0,0 +1,63 @@
+async function getSongs() {
+  try {
+    const response = await fetch("./Public/Music/music.json");
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching songs:", error);
+    return [];
+  }
+}
+
+
+async function main() {
+  let currentSong;
+  let songs = await getSongs();
+  console.log(songs);
+
+  // show all the songs int he playlist
+  let songUl = document
+    .querySelector(".song-list")
+    .getElementsByTagName("ul")[0];
+  console.log(songs);
+  for (const song of songs) {
+    songUl.innerHTML =
+      songUl.innerHTML +
+      ` <li class="flex gap-x-3 justify-between border rounded-md p-2 text-white
+      bg-gradient-to-r from-yellow-500 to-red-500">
+                            <img src="./img/music.svg" alt="">
+                            <span class="gap-x-2">${song.title}</span>
+                            <img src="./img/play.svg" alt="">
+                        </li>`;
+    console.log(songUl.innerHTML);
+  }
+
+  //Attach even listener to each song
+  Array.from(
+    document.querySelector(".song-list").getElementsByTagName("li")
+  ).forEach((e) => {
+    e.addEventListener("click", (event) => {
+      console.log(e.getElementsByTagName("span")[0].textContent);
+      playSong(e.getElementsByTagName("span")[0].textContent);
+    });
+  });
+}
+async function playSong(song) {
+  const songs = await getSongs();
+  console.log(song, "playsong ke andar");
+  for (const songObj of songs) {
+    console.log(song);
+    if (song === songObj.title) {
+      console.log("ayush");
+      let audio = new Audio("./Public/" + songObj.file);
+      console.log("./Public/" + songObj.file);
+      audio.play();
+      break;
+    }
+  }
+}
+document.addEventListener("DOMContentLoaded", () => {
+  main();
+});
